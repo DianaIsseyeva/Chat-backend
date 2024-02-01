b48abfa74023996dce276dffb485539f0f17629c
Diana <diana.dev777@gmail.com>
2024-02-02 00:30:35 +0600

    feat: implement abstract repository

---
+++ src/common/database/abstract.repository.ts
@@ -0,0 +1,58 @@
+import { Logger, NotFoundException } from '@nestjs/common';
+import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
+import { AbstractDocument } from './abstract.schema';
+// абстрактный класс предоставляет базовые операции для создания документов с использованием Mongoose,
+// ожидается, что он будет использоваться как базовый класс для репозиториев в NestJS-приложении.
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(protected readonly model: Model<TDocument>) {}
+
+  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const createdDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+    const savedDocument = await createdDocument.save();
+    return savedDocument as TDocument;
+  }
+
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    // при использовании lean(), возвращаемые объекты будут чистыми JavaScript-объектами без дополнительных методов и внутренней сложной структуры данных.
+    // lean<TDocument>() указывает Mongoose, что результат должен быть преобразован к типу TDocument после запроса.
+    const document = await this.model.findOne(filterQuery).lean<TDocument>();
+
+    if (!document) {
+      this.logger.warn('Document was not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document not found');
+    }
+    return document;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>,
+  ): Promise<TDocument> {
+    // Опция { new: true } указывает Mongoose вернуть обновленный документ после выполнения операции.
+    const document = await this.model
+      .findOneAndUpdate(filterQuery, update, {
+        new: true,
+      })
+      .lean<TDocument>();
+    if (!document) {
+      this.logger.warn('Document was not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document not found');
+    }
+    return document;
+  }
+
+  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
+    return this.model.find(filterQuery).lean<TDocument[]>();
+  }
+
+  async findOneAndDelete(
+    filterQuery: FilterQuery<TDocument>,
+  ): Promise<TDocument> {
+    return this.model.findOneAndDelete(filterQuery).lean<TDocument>();
+  }
+}
--- src/common/database/database.module.ts
+++ src/common/database/database.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-
+// создаем модуль базы данных для NestJS, используя Mongoose для подключения к MongoDB.
+// Настройки подключения читаются из переменных окружения с использованием ConfigService.
 @Module({
   imports: [
     MongooseModule.forRootAsync({

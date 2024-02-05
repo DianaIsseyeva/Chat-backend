cec8b77ec3a68f9991b705ea2b02e276d1fa6892
Diana <diana.dev777@gmail.com>
2024-02-05 10:05:30 +0600

    feat: users repository

--- src/common/database/abstract.repository.ts
+++ src/common/database/abstract.repository.ts
@@ -1,30 +1,30 @@
 import { Logger, NotFoundException } from '@nestjs/common';
 import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
-import { AbstractDocument } from './abstract.schema';
+import { AbstractEntity } from './abstract.entity';
 // абстрактный класс предоставляет базовые операции для создания документов
 // с использованием Mongoose,
 // ожидается, что он будет использоваться как базовый класс для репозиториев
 // в NestJS-приложении.
-export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+export abstract class AbstractRepository<T extends AbstractEntity> {
   protected abstract readonly logger: Logger;
 
-  constructor(protected readonly model: Model<TDocument>) {}
+  constructor(protected readonly model: Model<T>) {}
 
-  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+  async create(document: Omit<T, '_id'>): Promise<T> {
     const createdDocument = new this.model({
       ...document,
       _id: new Types.ObjectId(),
     });
     const savedDocument = await createdDocument.save();
-    return savedDocument as TDocument;
+    return savedDocument as T;
   }
 
-  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+  async findOne(filterQuery: FilterQuery<T>): Promise<T> {
     // при использовании lean(), возвращаемые объекты будут чистыми JavaScript-объектами
     // без дополнительных методов и внутренней сложной структуры данных.
-    // lean<TDocument>() указывает Mongoose, что результат должен быть преобразован
-    //  к типу TDocument после запроса.
-    const document = await this.model.findOne(filterQuery).lean<TDocument>();
+    // lean<T>() указывает Mongoose, что результат должен быть преобразован
+    //  к типу T после запроса.
+    const document = await this.model.findOne(filterQuery).lean<T>();
 
     if (!document) {
       this.logger.warn('Document was not found with filterQuery', filterQuery);
@@ -34,16 +34,16 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   }
 
   async findOneAndUpdate(
-    filterQuery: FilterQuery<TDocument>,
-    update: UpdateQuery<TDocument>,
-  ): Promise<TDocument> {
+    filterQuery: FilterQuery<T>,
+    update: UpdateQuery<T>,
+  ): Promise<T> {
     // Опция { new: true } указывает Mongoose вернуть обновленный документ
     // после выполнения операции.
     const document = await this.model
       .findOneAndUpdate(filterQuery, update, {
         new: true,
       })
-      .lean<TDocument>();
+      .lean<T>();
     if (!document) {
       this.logger.warn('Document was not found with filterQuery', filterQuery);
       throw new NotFoundException('Document not found');
@@ -51,13 +51,11 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
     return document;
   }
 
-  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
-    return this.model.find(filterQuery).lean<TDocument[]>();
+  async find(filterQuery: FilterQuery<T>): Promise<T[]> {
+    return this.model.find(filterQuery).lean<T[]>();
   }
 
-  async findOneAndDelete(
-    filterQuery: FilterQuery<TDocument>,
-  ): Promise<TDocument> {
-    return this.model.findOneAndDelete(filterQuery).lean<TDocument>();
+  async findOneAndDelete(filterQuery: FilterQuery<T>): Promise<T> {
+    return this.model.findOneAndDelete(filterQuery).lean<T>();
   }
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,18 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { AbstractEntity } from 'src/common/database/abstract.entity';
 
+// @Prop() - Этот декоратор применен к полям email и password класса UserDocument и указывает,
+// что они будут использоваться в схеме Mongoose.
+@Schema({ versionKey: false })
 @ObjectType()
-export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+export class User extends AbstractEntity {
+  @Prop()
+  @Field()
+  email: string;
+
+  @Prop()
+  password: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
